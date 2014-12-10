472b30154dd2fe22d45f32bdf74240d538a2b17c
lohanradu <lohan.radu@yahoo.com>
2014-12-10 17:02:07 +0200

    Update ArenaScript.cs

--- ArenaScript.cs
+++ ArenaScript.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class ArenaScript : MonoBehaviour {
 
 
-	void Start () {
+	void Start () {//edit
         //resize the arena so it fits the whole screen no matter the device
         float factor = Camera.main.orthographicSize;
 
