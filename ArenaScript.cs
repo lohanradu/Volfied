ace4e9ca4a8f2a321a6a60f905b2c980d22af507
Prodan Tudor <prodan_tudor@yahoo.com>
2014-12-10 17:01:35 +0200

    Update ArenaScript.cs

--- ArenaScript.cs
+++ ArenaScript.cs
@@ -1,1 +1,24 @@
-//
+using UnityEngine;
+using System.Collections;
+
+public class ArenaScript : MonoBehaviour {
+
+
+	void Start () {
+        //resize the arena so it fits the whole screen no matter the device
+        float factor = Camera.main.orthographicSize;
+
+        float factorX = (Screen.width * factor * 2) / Screen.height;
+        float factorY = factor * 2;
+
+        Vector2 tmpSize = transform.localScale;
+        tmpSize.x = factorX;
+        tmpSize.y = factorY;
+        transform.localScale = tmpSize;
+	}
+	
+	void Update () {
+	
+	}
+}
+
